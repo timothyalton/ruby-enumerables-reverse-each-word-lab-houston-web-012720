0918aa2f867fef599a39ac4df3869d1690c4ff06
Mitchell T Alton <mitchell.alton@live.com>
2020-01-25 01:58:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,6 @@ require 'pp'
 def reverse_each_word(sentence)
  word_array = sentence.split(" ")
  new_array = []
- new_array = word_array.collect.rev
+ new_array = word_array.collect{ |word| word.reverse }
+ new_array
 end
