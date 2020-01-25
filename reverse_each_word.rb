541301fcf82d9c88760f7c31c418936359ab4db6
Mitchell T Alton <mitchell.alton@live.com>
2020-01-25 02:00:36 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+require 'pp'
+
+def reverse_each_word(sentence)
+ word_array = sentence.split(" ")
+ new_array = []
+ new_array = word_array.collect{ |word| word.reverse }
+ new_array = new_array.join(" ")
+ new_array
+end
